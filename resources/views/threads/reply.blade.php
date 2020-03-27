2a48c9cdb78150f1bf0731cd64f4d22ed03a0276
Anton Diachkov <dyachkovanton13@gmail.com>
2020-03-27 09:23:32 +0200

    #8 user may publish threads + helpers

---
+++ resources/views/threads/reply.blade.php
@@ -0,0 +1,13 @@
+<div class="card" style="margin-bottom: 30px;">
+    <div class="card-header">
+        <a href="#">
+            {{$reply->owner->name}}
+        </a>
+        said {{$reply->created_at->diffForHumans()}}...
+
+    </div>
+
+    <div class="card-body">
+        {{$reply->body}}
+    </div>
+</div>
---
+++ resources/views/threads/show.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card" style="margin-bottom: 40px">
+                    <div class="card-header">
+                        <a href="#">{{$thread->creator->name}} </a> posted:
+                        {{$thread->title}}
+                    </div>
+
+                    <div class="card-body">
+                        {{$thread->body}}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+            </div>
+        </div>
+
+        @if(auth()->check())
+
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <form method="POST" action="{{$thread->path().'/replies'}}">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <textarea name="body" id="body" placeholder="Have something to say?"
+                                      class="form-control" rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="">Please <a href="{{route('login')}}">sign in</a> to participate in this discussion.</p>
+        @endif
+
+    </div>
+@endsection
+
